2b21e26855ec8f012153404fad11f2ccd68939a4
Junaid Q <xcoder27@gmail.com>
2017-08-05 02:54:14 +0500

    adding environment variables

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module Shopifyapi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(env_file)
 end
