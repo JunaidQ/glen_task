008591f386c642ce084833fc150b31b1c2a16bbc
Junaid Q <xcoder27@gmail.com>
2017-08-04 13:49:02 +0500

    setting up shopify api and getting the products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,19 @@
 class ProductsController < ApplicationController
   def index
+    @api_key = params[:search_box]
+    shop_url = "https://#{@api_key}:f93a66c64ef617e20dcdfa1c90ba7995@alibabalhr.myshopify.com/admin"
+    ShopifyAPI::Base.site = shop_url
+    
+    shop = ShopifyAPI::Shop.current
+    product_count = ShopifyAPI::Product.count
+    products = ShopifyAPI::Product.all
+    result_xml = products.to_xml(root: 'shop')
+    @result = result_xml
+
+    redirect_to do |format|
+      format.html
+      format.xml { render xml: @result}
+    end
+   
   end
 end
