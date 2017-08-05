5d9e884bcc26fedb73f71a97cd0b1493fbebb9b6
Junaid Q <xcoder27@gmail.com>
2017-08-05 11:15:52 +0500

    exception handling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,12 +8,17 @@ class ProductsController < ApplicationController
     shop_url = "https://#{@api_key}:f93a66c64ef617e20dcdfa1c90ba7995@alibabalhr.myshopify.com/admin"
     ShopifyAPI::Base.site = shop_url
     
-    shop = ShopifyAPI::Shop.current
-    product_count = ShopifyAPI::Product.count
-    products = ShopifyAPI::Product.all
-    result_xml = products.to_xml(root: 'shop')
-    @result = result_xml
+    begin 
+      shop = ShopifyAPI::Shop.current
+      product_count = ShopifyAPI::Product.count
+      products = ShopifyAPI::Product.all
+      result_xml = products.to_xml(root: 'shop')
+      @result = result_xml
 
+    rescue Exception
+      logger.error "Invalid Authentication Key "
+    end
+    
     redirect_to do |format|
       format.html
       format.xml { render xml: @result}
