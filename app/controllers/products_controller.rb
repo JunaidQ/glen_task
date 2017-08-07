31ae67acb16ba618467cd3c627ffc211c82a74e4
Junaid Q <xcoder27@gmail.com>
2017-08-07 17:52:23 +0500

    writing helpers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
-  
+  include ProductsHelper
+
   def index
   end
 
   def shop
-    @api_key = params[:search_box]
-    # shop_url = "https://#{@api_key}:f93a66c64ef617e20dcdfa1c90ba7995@alibabalhr.myshopify.com/admin"
-    shop_url = "https://#{@api_key}:#{ENV['PASSWORD']}@#{ENV['SHOP_NAME']}.myshopify.com/admin"
+    api_key = params[:search_box]
+    shop_url = get_shop_url(api_key)
     ShopifyAPI::Base.site = shop_url
     begin 
       shop = ShopifyAPI::Shop.current
@@ -21,39 +21,5 @@ class ProductsController < ApplicationController
       @result = result.to_xml
     end
     render xml: @result
-  end
-
-  def parse_result(result_xml) 
-    final_output = result_xml   
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.root {
-        xml.items {
-          result_xml.each do 
-            xml.item {
-              xml.sku   result_xml.first.variants.first.sku
-              xml.collection result_xml.first.product_type
-              xml.brand result_xml.first.vendor
-              xml.item_id    result_xml.first.id 
-              xml.barcode result_xml.first.variants.first.barcode
-              xml.title result_xml.first.title
-              xml.description result_xml.first.body_html
-              xml.price result_xml.first.variants.first.price
-              binding.pry
-              xml.tags do
-                result_xml.first.tags.split(",").each do |tag|
-                  xml.tag tag
-                end
-              end
-              xml.weight result_xml.first.variants.first.weight
-              xml.weight_unit result_xml.first.variants.first.weight_unit
-              xml.image_src result_xml.first.image.src
-              xml.inventory_quantity result_xml.first.variants.first.inventory_quantity
-            }
-          end
-        }
-      }
-    end
-    puts builder.to_xml
-    builder.to_xml
   end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,39 @@
 module ProductsHelper
+  def self.get_shop_url(str)
+    shopurl = "https://#{str}:#{ENV['PASSWORD']}@#{ENV['SHOP_NAME']}.myshopify.com/admin"
+    shopurl
+  end
+
+  def get_shop_url(str)
+    ProductsHelper.get_shop_url(str)
+  end
+
+  def parse_result(result_xml) 
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.items {
+        result_xml.each do 
+          xml.item {
+            xml.sku   result_xml.first.variants.first.sku
+            xml.collection result_xml.first.product_type
+            xml.brand result_xml.first.vendor
+            xml.item_id    result_xml.first.id 
+            xml.barcode result_xml.first.variants.first.barcode
+            xml.title result_xml.first.title
+            xml.description result_xml.first.body_html
+            xml.price result_xml.first.variants.first.price
+            xml.tags do
+              result_xml.first.tags.split(",").each do |tag|
+                xml.tag tag
+              end
+            end
+            xml.weight result_xml.first.variants.first.weight
+            xml.weight_unit result_xml.first.variants.first.weight_unit
+            xml.image_src result_xml.first.image.src
+            xml.stock_available result_xml.first.variants.first.inventory_quantity
+          }
+        end
+      }
+    end
+    builder.to_xml
+  end
 end
