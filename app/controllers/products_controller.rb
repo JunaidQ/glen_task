05533aa613232f2b730828db379e6edec9fae30d
Junaid Q <xcoder27@gmail.com>
2017-08-07 14:46:42 +0500

    some minor changes with controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,14 +7,12 @@ class ProductsController < ApplicationController
     @api_key = params[:search_box]
     shop_url = "https://#{@api_key}:f93a66c64ef617e20dcdfa1c90ba7995@alibabalhr.myshopify.com/admin"
     ShopifyAPI::Base.site = shop_url
-    
     begin 
       shop = ShopifyAPI::Shop.current
       product_count = ShopifyAPI::Product.count
       products = ShopifyAPI::Product.all
       result_xml = products.to_xml(root: 'shop')
       @result = parse_result(products)
-
     rescue Exception
       logger.error "Invalid Authentication Key "
       result = Nokogiri::XML::Builder.new do |xml|
@@ -22,29 +20,33 @@ class ProductsController < ApplicationController
       end
       @result = result.to_xml
     end
-    
     render xml: @result
   end
 
   def parse_result(result_xml) 
     final_output = result_xml   
-    binding.pry
-
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.root {
         xml.items {
           result_xml.each do 
             xml.item {
               xml.sku   result_xml.first.variants.first.sku
-              xml.id    result_xml.first.id 
+              xml.first_category result_xml.first.product_type
               xml.brand result_xml.first.vendor
+              xml.item_id    result_xml.first.id 
               xml.barcode result_xml.first.variants.first.barcode
               xml.title result_xml.first.title
-              xml.body_html result_xml.first.body_html
+              xml.description result_xml.first.body_html
               xml.price result_xml.first.variants.first.price
+              xml.tags do
+                result_xml.first.tags.each do
+                  xml.tag result_xml.first.title
+                end
+              end
               xml.weight result_xml.first.variants.first.weight
               xml.weight_unit result_xml.first.variants.first.weight_unit
               xml.image_src result_xml.first.image.src
+              xml.inventory_quantity result_xml.first.variants.first.inventory_quantity
             }
           end
         }
