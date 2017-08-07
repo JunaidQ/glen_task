9367374851b2cf0febcaa7078b15713a7c4f1aaa
Junaid Q <xcoder27@gmail.com>
2017-08-07 17:14:16 +0500

    completed parsing, code refactor

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,13 @@ class ProductsController < ApplicationController
 
   def shop
     @api_key = params[:search_box]
-    shop_url = "https://#{@api_key}:f93a66c64ef617e20dcdfa1c90ba7995@alibabalhr.myshopify.com/admin"
+    # shop_url = "https://#{@api_key}:f93a66c64ef617e20dcdfa1c90ba7995@alibabalhr.myshopify.com/admin"
+    shop_url = "https://#{@api_key}:#{ENV['PASSWORD']}@#{ENV['SHOP_NAME']}.myshopify.com/admin"
     ShopifyAPI::Base.site = shop_url
     begin 
       shop = ShopifyAPI::Shop.current
       product_count = ShopifyAPI::Product.count
       products = ShopifyAPI::Product.all
-      result_xml = products.to_xml(root: 'shop')
       @result = parse_result(products)
     rescue Exception
       logger.error "Invalid Authentication Key "
@@ -31,16 +31,17 @@ class ProductsController < ApplicationController
           result_xml.each do 
             xml.item {
               xml.sku   result_xml.first.variants.first.sku
-              xml.first_category result_xml.first.product_type
+              xml.collection result_xml.first.product_type
               xml.brand result_xml.first.vendor
               xml.item_id    result_xml.first.id 
               xml.barcode result_xml.first.variants.first.barcode
               xml.title result_xml.first.title
               xml.description result_xml.first.body_html
               xml.price result_xml.first.variants.first.price
+              binding.pry
               xml.tags do
-                result_xml.first.tags.each do
-                  xml.tag result_xml.first.title
+                result_xml.first.tags.split(",").each do |tag|
+                  xml.tag tag
                 end
               end
               xml.weight result_xml.first.variants.first.weight
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Shopifyapi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    config.action_view.automatically_disable_submit_tag = false
 
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
